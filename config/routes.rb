5ac966ef73933ed890f824e743ed0170eaaadf14
Bennett Foster <bennfos@gmail.com>
2020-02-02 16:45:49 -0600

    remove params from book routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :pages
   resources :quotes
   resources :users
-  resources :books, param: :title
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
