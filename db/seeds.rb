902fc3a6c577d2534fbc3d79a4f9effe8646f79d
Bennett Foster <bennfos@gmail.com>
2020-02-02 16:32:06 -0600

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Book.create(title: 'test book', description: 'testing', starts_blank: true, user_id: 1)
+
+Page.create(month: 'january', day:'1', thought:'test thought', book_id: 1)
+
+Quote.create(quote_text: 'test text', quote_author: 'tester', page_id: 1)
