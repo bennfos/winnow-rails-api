631a66472e697a5af4d3d636019330971a2491ac
Bennett Foster <bennfos@gmail.com>
2020-02-02 11:34:06 -0600

    add properties and methods to book, page and quote models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,16 @@
 class Book < ApplicationRecord
+  has_many :pages
+
+  before_save :downcase_name
+
+  validates :title, presence: true
+
+  def to_param
+    title
+  end
+
+  def downcase_name
+    self.name.downcase!
+  end
+
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,13 @@
 class Page < ApplicationRecord
+  belongs_to :book, dependent: :destroy
+
+  has_many :quotes
+
+  def to_param
+    "#{month}-#{day}"
+  end
+
+  def downcase_name
+    self.name.downcase!
+  end
 end
