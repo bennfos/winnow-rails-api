f6fac0cc3eef445dfb064fe888bbda7b702eb2b4
Bennett Foster <bennfos@gmail.com>
2020-02-05 17:07:37 -0500

    changed cors origin allowances, added password to users with migration

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module WinnowRailsApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://winnow-client.herokuapp.com'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
