5a782fcd165d5cd0a46cb3653e59920449179905
Bennett Foster <bennfos@gmail.com>
2020-02-06 16:28:51 -0500

    user controller commented out

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,33 +1,33 @@
 module Api::V1
   class UsersController < ApplicationController
-    has_secure_password
+    # has_secure_password
     before_action :set_user, only: [:show, :update, :destroy]
 
-    # GET /users
-    def index
-      @users = User.all
+    # # GET /users
+    # def index
+    #   @users = User.all
 
-      render json: @users
-    end
-
-
-
-    # POST /users
-    def create
-      user = User.create(user_params)
-
-      if user.valid?
-        payload = {user_id: user.id}
-        token = encode_token(payload)
-        render json: {user: user, jwt: token}
-      else
-        render json: {errors: user.errors.full_messages}, status: :not_acceptable
-      end
-    end
+    #   render json: @users
+    # end
 
 
 
-    private
+    # # POST /users
+    # def create
+    #   user = User.create(user_params)
+
+    #   if user.valid?
+    #     payload = {user_id: user.id}
+    #     token = encode_token(payload)
+    #     render json: {user: user, jwt: token}
+    #   else
+    #     render json: {errors: user.errors.full_messages}, status: :not_acceptable
+    #   end
+    # end
+
+
+
+    # private
 
 
       # Only allow a trusted parameter "white list" through.
