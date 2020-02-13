b827b31d90c2ee8f11607e8747c6398176208c7b
Bennett Foster <bennfos@gmail.com>
2020-02-12 20:18:21 -0600

    uncomment elsif

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -7,7 +7,7 @@ module Api::V1
       if params[:page_id]
         @quotes = Quote.where(page_id: params[:page_id])
         render json: @quotes
-      # elsif params[:search]
+      elsif params[:search]
         @quotes = Quote.includes(:page).where("page.month" => params[:search]
           .or(Quote.includes(:page).where("page.thought" => params[:search])
           .or(Quote.includes(:page).where(quote_text: params[:search]))
