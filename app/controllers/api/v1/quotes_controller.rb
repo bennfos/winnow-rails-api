0efe5e78e4ecf3e6339455d092d619246107b4fa
Bennett Foster <bennfos@gmail.com>
2020-02-13 01:43:05 -0600

    present?

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -4,14 +4,14 @@ module Api::V1
 
     # GET /quotes
     def index
-      if params[:search]
+      if params[:search].present?
         @quotes =
-        Quote.joins(:page).where("page.month LIKE ?", "%#{params[:search]}%")
-          .or(Quote.joins(:page).where("page.thought LIKE ?", "%#{params[:search]}%"))
-          .or(Quote.joins(:page).where("quote_text LIKE ?", "%#{params[:search]}%"))
-          .or(Quote.joins(:page).where("quote_author LIKE ?", "%#{params[:search]}%"))
+        Quote.includes(:page).where("page.month LIKE ?", "%#{params[:search]}%")
+          .or(Quote.includes(:page).where("page.thought LIKE ?", "%#{params[:search]}%"))
+          .or(Quote.includes(:page).where("quote_text LIKE ?", "%#{params[:search]}%"))
+          .or(Quote.includes(:page).where("quote_author LIKE ?", "%#{params[:search]}%"))
         render json: @quotes
-      elsif params[:page_id]
+      elsif params[:page_id].present?
         @quotes = Quote.where(page_id: params[:page_id])
         render json: @quotes
       else
