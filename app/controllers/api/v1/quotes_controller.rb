2719a97a8137066bc804392d3f5e7ee0b7f79530
Bennett Foster <bennfos@gmail.com>
2020-02-12 23:23:56 -0600

    change order

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -4,14 +4,14 @@ module Api::V1
 
     # GET /quotes
     def index
-      if params[:page_id]
-        @quotes = Quote.where(page_id: params[:page_id])
-        render json: @quotes
-      elsif params[:search]
-        @quotes = Quote.includes(:page).where("page.month LIKE ?",  "%#{params[:search]}%")
+      if params[:search]
+        @quotes = Quote.includes(:page).where("page.month LIKE ?", "%#{params[:search]}%")
           .or(Quote.includes(:page).where("page.thought LIKE ?", "%#{params[:search]}%"))
           .or(Quote.includes(:page).where("quote_text LIKE ?", "%#{params[:search]}%"))
           .or(Quote.includes(:page).where("quote_author LIKE ?", "%#{params[:search]}%"))
+        render json: @quotes
+      elsif params[:page_id]
+        @quotes = Quote.where(page_id: params[:page_id])
         render json: @quotes
       else
         @quotes = Quote.all()
