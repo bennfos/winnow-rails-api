af73c5d3036a4d89f7501091f7cbb313b57878ab
Bennett Foster <bennfos@gmail.com>
2020-02-14 16:13:44 -0600

    only to only: in session route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :sessions, only [:create]
+      resources :sessions, only: [:create]
       resources :users
       resources :books
       resources :pages
