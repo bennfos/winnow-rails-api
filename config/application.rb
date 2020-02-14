99d2e0f7e4342b0e5591330e9a5a23074b8f4ca8
Bennett Foster <bennfos@gmail.com>
2020-02-14 13:08:00 -0600

    change origin from * to winnow-client in cors.rb

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module WinnowRailsApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.use ActionDispatch::Cookies
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://winnow-client.herokuapp.com'
 
     resource '*',
       headers: :any,
