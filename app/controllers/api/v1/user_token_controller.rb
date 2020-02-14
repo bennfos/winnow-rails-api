42312b87044c3a18096eaf970b1ce1ea9a244a3d
Bennett Foster <bennfos@gmail.com>
2020-02-14 13:40:51 -0600

    comment out skip token user token controller

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -1,5 +1,5 @@
 module Api::V1
   class UserTokenController < Knock::AuthTokenController
-    skip_before_action :verify_authenticity_token
+    # skip_before_action :verify_authenticity_token
   end
 end
