49f11d16fdf8823dd512f78d9411175067f863e9
Bennett Foster <bennfos@gmail.com>
2020-02-14 14:36:46 -0600

    new user model and migration

--- db/migrate/20200214203408_create_users.rb
+++ db/migrate/20200214203408_create_users.rb
@@ -5,10 +5,9 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :username
       t.string :email
-      t.string :password
-      t.string :password_confirmation
+      t.string :username
+      t.string :password_digest
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_210252) do
+ActiveRecord::Schema.define(version: 2020_02_14_203408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,10 +44,9 @@ ActiveRecord::Schema.define(version: 2020_02_13_210252) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "username"
     t.string "email"
-    t.string "password"
-    t.string "password_confirmation"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
