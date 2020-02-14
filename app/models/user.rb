df44510fecc0532df3dd675f5ede1a0bf06abe29
Bennett Foster <bennfos@gmail.com>
2020-02-14 14:45:29 -0600

    user model has_secure_password and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_presence_of :username
+  validates_uniqueness_of :email
+
+
 end
