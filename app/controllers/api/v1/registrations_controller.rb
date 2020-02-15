dd14563bf8c37f1fc592fee8bd450baaf84475db
Bennett Foster <bennfos@gmail.com>
2020-02-14 18:51:09 -0600

    vid 3 to 5:04

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,27 @@
+module Api::V1
+  class RegistrationsController < ApplicationController
+    def create
+      user = User.create!(
+        first_name: params['first_name'],
+        last_name: params['last_name'],
+        email: params['email'],
+        username: params['username'],
+        password: params['password']
+        password_confirmation: params['password_confirmation']
+      )
+
+      if user
+        session[:user_id] = user.id
+        render json: {
+          status: :created,
+          user: user
+        }
+      else
+        render json: {
+          status: 500
+        }
+      end
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :sessions, only: [:create]
+      resources :registrations, only: [:create]
       resources :users
       resources :books
       resources :pages
