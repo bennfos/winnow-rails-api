9e6f46d2a5ce937964d71a57d4b27b6cbcc60145
Bennett Foster <bennfos@gmail.com>
2020-02-16 07:23:45 -0600

    session store url added https://

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: '_winnow_client', domain: "
-winnow-rails-api.herokuapp.com"
+Rails.application.config.session_store :cookie_store,
+key: "_winnow_client", domain: "https://winnow-rails-api.herokuapp.com"
