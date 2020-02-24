9ed36ffee01af8a67a894efe8f92d772cfb9827d
Bennett Foster <bennfos@gmail.com>
2020-02-24 07:33:22 -0600

    short session and expire_after

--- app/controllers/concerns/application_controller.rb
+++ app/controllers/concerns/application_controller.rb
@@ -3,47 +3,16 @@ class ApplicationController < ActionController::Base
 
   include ::ActionController::Cookies
 
+  after_filter :short_session
+
+  def short_session
+    request.session_options = request.session_options.dup
+    request.session_options[:expire_after] = 14.days
+    request.session_options.freeze
+  end
+
   def current_user
     User.find_by(id: session[:user_id])
   end
-
-#   def encode_token(payload)
-#     JWT.encode(payload, 'my_secret')
-#   end
-
-#   def session_user
-#     decoded_hash = decoded_token()
-#     if !decoded_hash.empty?
-#       user_id = decoded_hash[0]['user_id']
-#       @user = User.find_by(id: user_id)
-#     else
-#       nil
-#     end
-#   end
-
-# def auth_header
-#   request.headers['Authorization']
-# end
-
-# def decoded_token
-#   if auth_header
-#     token = auth_header.split(' ')[1]
-#     begin
-#       JWT.decode(token, 'my_secret', true, algorithm: 'HS256')
-#     rescue JWT::DecodeError
-#       []
-#     end
-#   end
-# end
-
-#   before_action :require_login
-
-#   def logged_in?
-#     !!session_user
-#   end
-
-#   def require_login
-#     render json: {message: 'please login'}, status: :unauthorized unless logged_in?
-#   end
 
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 Rails.application.config.session_store :cookie_store,
-key: "_winnow_client", domain: "https://winnow-rails-api.herokuapp.com"
+key: "_winnow_client", domain: "https://winnow-rails-api.herokuapp.com", expire_after: 14.days
